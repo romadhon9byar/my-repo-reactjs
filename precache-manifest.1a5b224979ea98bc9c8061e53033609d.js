a9eb4890e33f7b9e09d85f302a3548112f0ae320
Ahmad Romadhon <romadhon9byar@gmail.com>
2019-07-17 01:04:35 +0700

    Updates

---
+++ precache-manifest.1a5b224979ea98bc9c8061e53033609d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b393481839f4232bdaec3db96c4af1",
+    "url": "/my-repo-reactjs/index.html"
+  },
+  {
+    "revision": "d8337b42b9ba2c754f85",
+    "url": "/my-repo-reactjs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-repo-reactjs/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d8337b42b9ba2c754f85",
+    "url": "/my-repo-reactjs/static/js/main.76168fd2.chunk.js"
+  },
+  {
+    "revision": "51ae42f2edfe7608292b",
+    "url": "/my-repo-reactjs/static/js/runtime~main.2b63a38d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-repo-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
